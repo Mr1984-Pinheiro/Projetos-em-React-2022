4f53e75a2ff40d6d52e2556124ba9a865ca1c224
Eduardo <cadutapinheiro@gmail.com>
2022-10-29 05:14:26 -0300

    install react hooks form

--- formulario/src/App.js
+++ formulario/src/App.js
@@ -1,11 +1,53 @@
 
 import './App.css';
+import logo from './assets/logo.png'
+import alert from './assets/alert.png'
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+  function onSubmit(userData) {
+    console.log(userData)
+  }
+
   return (
-    <div >
-      Hello react
-    </div>
+
+    <form onSubmit={handleSubmit(onSubmit)} >
+      <div className="form-title" >
+
+      <img src={logo} alt="imagem-logo" />
+      <h1>Criar conta</h1>
+      </div>
+
+      <label>
+        Nome
+        <input {...register("name", {required: true})} />
+        {errors.name && 
+        <div className='span-name'>
+            <img className='alert' src={alert} alt="imagem-exlamação" />
+        <span>Digite o nome</span>
+        </div>       
+        }
+      </label>
+
+      <label>
+        Email
+        <input {...register("email")} />
+      </label>
+
+      <label>
+        Senha
+        <input {...register("password")} />
+      </label>
+
+      <label>
+        Confirmar senha
+        <input {...register("confirmPassword")} />
+      </label>
+
+      <button type='submit'>Cadastrar-se</button>
+    </form>
   );
 }
 
